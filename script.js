7098875c36a2066dbbda86231c1bd30aee345a6d
Pedro_Zaustt <147394862+ZTPedroDaniel@users.noreply.github.com>
2025-08-26 17:45:29 -0300

    .

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Voce acorda normalmente um dia, e percebe que se tornou um Cybertroniano, agora voce deve decidir sua facção na grande guerra de Cybertron.",
+        enunciado: "Você desperta como um Transformer em meio às ruínas de uma cidade cybertroniana destruída pela guerra. Um grupo de Autobots e outro de Decepticons passam lutando. O que você faz?",
         alternativas: [
             {
-                texto: "Se tornar um Autobot.",
-                afirmacao: "Decidiu seguir as ideologias de Optimus Prime. "
+                texto: "Se esconde para observar melhor.",
+                afirmacao: "Ao despertar, decidiu se esconder e observar o conflito."
             },
             {
-                texto: "Se tornar um Decepticon",
-                afirmacao: "Decidiu seguir as ideologias de Megatron."
+                texto: "Corre para ajudar um dos lados, mesmo sem saber quem é quem.",
+                afirmacao: "Ao despertar, decidiu correr para ajudar no conflito."
             }
         ]
     },
     {
-        enunciado: "Voce acaba se unindo a uma equipe da sua facção, e fica diante dos seus aliados, o que voce faz?",
+        enunciado: "Enquanto caminha, você encontra dois Transformers diferentes: Um Autobot que fala sobre liberdade e proteger os inocentes. Um Decepticon que fala sobre poder e restaurar a glória de Cybertron. Quem você ouve primeiro?",
         alternativas: [
             {
-                texto: "Conversar com eles.",
-                afirmacao: "Conversou com seus aliados, os conhecendo melhor e criando laços."
+                texto: "O Autobot.",
+                afirmacao: "Durante o conflito, decidiu ouvir o Autobot."
             },
             {
-                texto: "Fico na minha.",
-                afirmacao: "Ficou no seu canto sozinho apenas observando."
+                texto: "O Decepticon.",
+                afirmacao: "Durante o conflito, decidiu ouvir o Decepticon."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Durante a exploração, você descobre que pode transformar-se pela primeira vez. Em que forma prefere assumir?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Um veículo rápido, feito para reconhecimento.",
+                afirmacao: "Durante uma exploração, decidiu se transformar em um veiculo rápido para exploração."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Um veículo pesado, feito para combate.",
+                afirmacao: "Durante uma exploração, decidiu se transformar em um veículo pesado para combate."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Um grupo de drones neutros pede sua ajuda para fugir da zona de guerra. Como você age?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Ajuda-os a escapar, mesmo que isso atrase sua jornada.",
+                afirmacao: "Ajudou drones neutros a fugir de uma zona de combate."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Ignora-os e segue adiante, focado no seu próprio destino.",
+                afirmacao: "Ignorou drones neutros a fugir de uma zona de combate."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Chega o momento de escolher. Os Autobots e os Decepticons te encontram ao mesmo tempo, cada lado estende a mão e pede sua lealdade.Qual caminho você decide seguir?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Juntar-se aos Autobots e lutar pela liberdade.",
+                afirmacao: "Acabou se tornando um heróico Autobot."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Juntar-se aos Decepticons e lutar pelo poder.",
+                afirmacao: "Acabou se tornando um maligno Decepticon."
             }
         ]
     },
